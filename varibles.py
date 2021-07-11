90db09b1ef47a4a2eb973732d215971afd37c71c
CodePalOffical <codepaloffical@gmail.com>
2021-07-11 15:22:25 -0500

    Adding variables files

---
+++ varibles.py
@@ -0,0 +1,58 @@
+
+# Strings
+name = "PalCode"
+
+# print("My name is " + name)
+
+# Numbers 
+
+x = 5
+
+y = 12
+
+z = x # 5
+
+# total = 22 
+total = x + y + z 
+
+# print("Value of x = " + str(x))
+# print("Value of y = " + str(y))
+# print("Value of z = " + str(z))
+# print("Value of total = " + str(total))
+
+
+# Bool or Boolean
+
+# status = True
+
+# print("Value of Status = " + str(status))
+
+# status = False 
+
+# print("Value of Status = " + str(status))
+
+
+# List 
+
+fruits = ["apples", "oranges", "bananas", "mangos"]
+newList = [[1, 2, 4], [2, 9, 8], [1, 8, 6]]
+# print(fruits)
+# print(newList)
+
+# print("lenght of the fruits list is " + str(len(fruits)))
+# print("lenght of the newList list is " + str(len(newList)))
+
+# Index 
+
+car = "Hotwheels"
+
+print("lenght of car is " + str(len(car)))
+
+print(car[3-1])
+print(car[0])
+
+newList = [[1, 2, 4, 5], [2, 9, 8], [1, 8, 6]]
+
+print("Length of the list is " + str(len(newList)))
+
+print("Lenght of the first list in newList is " + str(len(newList[0])))
